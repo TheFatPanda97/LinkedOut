96d276a7b3baa5c60b594ee16e5cfdcc247d8f49
TheFatPanda97 <shawnhu97@gmail.com>
2019-02-23 10:27:56 -0500

    Shawn - got calendar working

--- app/src/main/java/ca/utoronto/shawnhu/linkedout/MainActivity.java
+++ app/src/main/java/ca/utoronto/shawnhu/linkedout/MainActivity.java
@@ -1,5 +1,8 @@
 package ca.utoronto.shawnhu.linkedout;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,15 +15,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    CompactCalendarView compactCalendar;
+    TextView txtDisplay;
+    private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMM yyyy", Locale.getDefault());
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -40,6 +59,37 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        txtDisplay = findViewById(R.id.txt_event);
+
+        compactCalendar = findViewById(R.id.compactcalendar_view);
+        compactCalendar.setUseThreeLetterAbbreviation(true);
+
+        final Event ev1 = new Event(Color.BLUE, 1550754000000L, "Fuck I don't know");
+        compactCalendar.addEvent(ev1);
+        final Event ev2 = new Event(Color.RED, 1550840400000L, "Fuck I don't know again");
+        compactCalendar.addEvent(ev2);
+
+        compactCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+
+                if (dateClicked.toString().compareTo("Thu Feb 21 00:00:00 EST 2019") == 0) {
+                    txtDisplay.setText(ev1.getData().toString());
+                }else if (dateClicked.toString().compareTo("Fri Feb 22 00:00:00 EST 2019") == 0) {
+                    txtDisplay.setText(ev2.getData().toString());
+                } else {
+                    txtDisplay.setText("No Events Planned for that day");
+                }
+
+
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                toolbar.setTitle(dateFormatMonth.format(firstDayOfNewMonth));
+            }
+        });
     }
 
     @Override
@@ -85,6 +135,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
+
 
         } else if (id == R.id.nav_manage) {
 
